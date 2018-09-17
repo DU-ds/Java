8a3166b99814a9d37d9ab1fcdb01c30b10116d04
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2018-09-17 16:34:30 -0400

    M  IndexError.java ?? Add.class ?? Add.java ?? ArithmeticDemo.class ?? ArithmeticDemo.java ?? Card.java ?? DoWhileDemo.class ?? DoWhileDemo.java ?? Hailstone.class ?? IndexError.class ?? PrintFormattingPractice.java ?? PrintFormattingPractice2.java ?? PrintFormattingPractice3.class ?? PrintFormattingPractice3.java ?? Test1091618.class ?? Test1091618.java ?? WhileDemo.class ?? WhileDemo.java

--- IndexError.java
+++ IndexError.java
@@ -10,7 +10,7 @@ public class IndexError
         int [] arr = {0,1,2,3,4,5,6,7,8,9};
  
 
-        for (int j = 0; j < (n +1); j++ )  // expection thrown b/c last iteration of loop is out of bounds
+        for (int j = 0; j < (n +1); j++ )  // exception thrown b/c last iteration of loop is out of bounds
         {
             arr[j] = j; 
             System.out.println(arr[j]);
