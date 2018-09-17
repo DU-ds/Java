b37d8a20b06f173d0ebef69c8cabf19d401e1a05
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2018-09-17 18:30:33 -0400

    M  ListOfNumbers.java ?? Add.class ?? Add.java ?? ArithmeticDemo.class ?? ArithmeticDemo.java ?? Card.java ?? DoWhileDemo.class ?? DoWhileDemo.java ?? Hailstone.class ?? IndexError.class ?? PrintFormattingPractice.java ?? PrintFormattingPractice2.java ?? PrintFormattingPractice3.class ?? PrintFormattingPractice3.java ?? Test1091618.class ?? Test1091618.java ?? WhileDemo.class ?? WhileDemo.java

--- ListOfNumbers.java
+++ ListOfNumbers.java
@@ -2,31 +2,55 @@
   *  https://docs.oracle.com/javase/tutorial/essential/exceptions/handling.html
   * 
   **/
-// Note: This class will not compile yet.
+  // Note: This class will not compile yet.
 import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
 
-public class ListOfNumbers {
+public class ListOfNumbers 
+{
 
     private List<Integer> list;
     private static final int SIZE = 10;
 
-    public ListOfNumbers () {
+    public ListOfNumbers () 
+    {
         list = new ArrayList<Integer>(SIZE);
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) 
+        {
             list.add(new Integer(i));
         }
     }
 
-    public void writeList() {
+    public void writeList() 
+    {
     // The FileWriter constructor throws IOException, which must be caught.
-        PrintWriter out = new PrintWriter(new FileWriter("OutFile.txt"));
+        PrintWriter out = 0 // the tutorial says to use null, not 0
+       try
+       {
 
-        for (int i = 0; i < SIZE; i++) {
+            PrintWriter out = new PrintWriter(new FileWriter("OutFile.txt"));
+
+            for (int i = 0; i < SIZE; i++) {
             // The get(int) method throws IndexOutOfBoundsException, which must be caught.
             out.println("Value at: " + i + " = " + list.get(i));
+            }
+        
+        
+        } catch (IOException er) 
+        {   System.err.println("IOException :" + er.getMessage() );
+
         }
-        out.close();
+
+        catch (IndexOutOfBoundsException er) 
+        {   System.err.println("IndexOutOfBoundsException: " + er.getMessage() );
+
+        } finally{
+            if( out != 0)  // update to null if out is declared as null instead of 0
+            { 
+            out.close();  //I omitted the print statements
+            }
+        }
     }
 }
+
