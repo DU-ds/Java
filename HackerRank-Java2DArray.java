1943ed24f1ab9584f777c6f347da345d1fe4dd81
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-04-16 15:29:29 -0400

    "Working version"

--- HackerRank-Java2DArray.java
+++ HackerRank-Java2DArray.java
@@ -1,76 +1,69 @@
-/*answer to 
-https://www.hackerrank.com/challenges/java-2d-array/problem?h_r=next-challenge&h_v=zen
-*/
-//change file name to compile!!!
+    /*answer to 
+    https://www.hackerrank.com/challenges/java-2d-array/problem?h_r=next-challenge&h_v=zen
+    */
+    //change file name to compile!!!
 
 
-/*
-promt:
-You are given a 6 * 6 2D array. An hourglass in an array is a portion shaped like this:
+    /*
+    promt:
+    You are given a 6 * 6 2D array. An hourglass in an array is a portion shaped like this:
 
-a b c
-  d
-e f g
+    a b c
+      d
+    e f g
 
-For example, if we create an hourglass using the number 1 within an array full of zeros, it may look like this:
+    For example, if we create an hourglass using the number 1 within an array full of zeros, it may look like this:
 
-1 1 1 0 0 0
-0 1 0 0 0 0
-1 1 1 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
+    1 1 1 0 0 0
+    0 1 0 0 0 0
+    1 1 1 0 0 0
+    0 0 0 0 0 0
+    0 0 0 0 0 0
+    0 0 0 0 0 0
 
-Actually, there are many hourglasses in the array above. The three leftmost hourglasses are the following:
+    Actually, there are many hourglasses in the array above. The three leftmost hourglasses are the following:
 
-1 1 1     1 1 0     1 0 0
-  1         0         0
-1 1 1     1 1 0     1 0 0
+    1 1 1     1 1 0     1 0 0
+      1         0         0
+    1 1 1     1 1 0     1 0 0
 
-The sum of an hourglass is the sum of all the numbers within it. The sum for the hourglasses above are 7, 4, and 2, respectively.
+    The sum of an hourglass is the sum of all the numbers within it. The sum for the hourglasses above are 7, 4, and 2, respectively.
 
-In this problem you have to print the largest sum among all the hourglasses in the array.
+    In this problem you have to print the largest sum among all the hourglasses in the array.
 
-Input Format
+    Input Format
 
-There will be exactly
-lines, each containing integers seperated by spaces. Each integer will be between and
+    There will be exactly 6 lines, each containing 6 integers seperated by spaces. Each integer will be between 9 and -9 inclusive.
 
-inclusive.
+    Output Format
 
-Output Format
+    Print the answer to this problem on a single line.
 
-Print the answer to this problem on a single line.
+    Sample Input
 
-Sample Input
+    1 1 1 0 0 0
+    0 1 0 0 0 0
+    1 1 1 0 0 0
+    0 0 2 4 4 0
+    0 0 0 2 0 0
+    0 0 1 2 4 0
 
-1 1 1 0 0 0
-0 1 0 0 0 0
-1 1 1 0 0 0
-0 0 2 4 4 0
-0 0 0 2 0 0
-0 0 1 2 4 0
+    Sample Output
 
-Sample Output
+    19
 
-19
+    Explanation
 
-Explanation
+    The hourglass which has the largest sum is:
 
-The hourglass which has the largest sum is:
+    2 4 4
+      2
+    1 2 4
+    */
 
-2 4 4
-  2
-1 2 4
-*/
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
+
 
 public class Solution {
 
@@ -94,16 +87,17 @@ public class Solution {
         scanner.close();
 
 
-        int max = ;
-        for(int i = 0; i < 6; i++)
+        int max = 6 * 6 * -9; //6 * 6 array, -9 min value
+        //so max is initiallized to be the lowest possible sum of the entire array, which is less than any possilbe hourglass sum
+        for(int i = 0; i < 4; i++)
         {
-            for(int j = 0; j < 6; j++)
+            for(int j = 0; j < 4; j++)
             {
-                if(isValidHourglassIndex(i,j))
+                //if(isValidHourglassIndex(i,j))  //needed if looping over whole array.
                 {
                     int[] h = getHourglass(arr, i, j);
                     int sum = sumOfHourglass(h);
-
+                    //System.out.println("i+j: " + (i+j) + " h[0]: " + h[0] + " " + sum);
 
                     if(sum > max)
                     {
@@ -122,8 +116,7 @@ public class Solution {
     */
 
     /*
-    @param i - column index
-    @param j - row index
+    @param i,j - integer representing column / row index
     @return - true iff i and j are the lowest integer indexes of a valid three by three array in the six by six array (arr).
     */
     static boolean isValidHourglassIndex(int indexi, int indexj)
@@ -155,8 +148,9 @@ public class Solution {
 
     /* int[] getHourglass(int[][] array, int indexi, int indexj)
     get hourglass starting from indexi and index j where 
-    @param indexi,indexj - i and j are the lowest integer indexes of a valid hourglass in the 2d array.
-        Note: i indexes columns, j indexes rows.
+
+    @param indexi - lowest integer row index of a valid hourglass in the 2d array.
+    @param indexj - lowest integer column index of a valid hourglass in the 2d array.
     @return - returns an int array containing the elements of the hourglass
     */
 
@@ -173,17 +167,19 @@ public class Solution {
             {
                 if(j == 1) //middle row
                 {
-                    if(j == i)//for j = 1 should only add i=j (e.g. middle int in middle row)
+                    if(j == i)// j = 1 should only add i=j (e.g. middle int in middle row)
                     {
-                        a[ai] = array[j][i];
+                        a[ai] = threebythree[j][i];
                         ai++;
+                        
+
                     }
-                    //dont increase ai b/c no element to add
+                    else{}//dont increase ai b/c no element to add
                 }
 
                 else
                 {
-                    a[ai] = array[j][i];
+                    a[ai] = threebythree[j][i];
                     ai++;
                 }
             }
@@ -196,7 +192,8 @@ public class Solution {
 
         get 3 x 3 array - indexi and indexj are the lowest index elements of array in return
 
-        @param indexi,indexj - row and column index of corner of return
+        @param indexi - row    index of corner of return
+        @param indexj - column index of corner of return
 
         @return 3 x 3 array from array
     */
@@ -208,7 +205,9 @@ public class Solution {
         {
             for(int j = 0; j < 3; j++)
             {
-                a[i][j] = array[indexj + j][indexi + i];
+                int ival = indexi + i; 
+                int jval = indexj + j;
+                a[i][j] = array[ival][jval];
             }
         }
 
