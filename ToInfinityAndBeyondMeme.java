1c36f0ffda284f584db4a23a4e2e4482eb541bcf
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-07-31 10:54:59 -0400

    from a meme on facebook in group Computer Science #{memes} for #{typeOfStudent}

---
+++ ToInfinityAndBeyondMeme.java
@@ -0,0 +1,8 @@
+public class ToInfinityAndBeyondMeme{
+    
+    public static void main(String[] argv){
+        for(int i = 0; i >= 0; i++){
+            System.out.println(i);
+        }
+    }
+}
